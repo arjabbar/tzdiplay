859540b5981f61da8597ac1e2393ee5b9f5a2b72
Abdur Jabbar <arjabbar@yahoo.com>
2019-10-26 13:48:04 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace DisplaySystemTimeZones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZoneInfo.GetSystemTimeZones().ToJson().Print();
+        }
+    }
+}
